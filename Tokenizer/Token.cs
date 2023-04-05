322f0fc7585d749320135bda779fd05b9355239d
AmirSina Mashayekh <amirsina.mashayekh@outlook.com>
2023-04-05 15:13:11 +0330

    Added Token class

---
+++ Tokenizer/Token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokenizer
+{
+    internal class Token
+    {
+        public TokenType Type { get; }
+
+        public string Value { get; }
+
+        public int Line { get; }
+
+        public int Column { get; }
+
+        public Token(TokenType type, string value, int line, int column)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Value = value;
+            Line = line;
+            Column = column;
+        }
+    }
+}
