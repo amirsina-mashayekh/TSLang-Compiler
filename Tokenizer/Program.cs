2778547b608905230f925719dede0cc91107909f
AmirSina Mashayekh <amirsina.mashayekh@outlook.com>
2023-04-17 14:42:10 +0330

    Simplified Main code of Tokenizer

--- Tokenizer/Program.cs
+++ Tokenizer/Program.cs
@@ -4,13 +4,7 @@ string path = args.Length == 0 ? Console.ReadLine()! : args[0];
 
 TesLangTokenizer tokenizer = new(new StreamReader(path));
 
-List<Token> tokens = new();
 while (!tokenizer.EndOfStream)
 {
-    tokens.Add(tokenizer.NextToken());
-}
-
-foreach (var token in tokens)
-{
-    Console.WriteLine(token);
+    Console.WriteLine(tokenizer.NextToken());
 }
